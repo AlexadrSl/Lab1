24ed9fc025ff97dd050d66797d0f3e831a28e805
AlexadrSl <93402752+AlexadrSl@users.noreply.github.com>
2022-03-23 14:13:23 +0300

    Add files via upload

---
+++ lab1/lab1.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student() { Age = 20, Id = 7, GroupNumber = 31, Name = "Семён", Surname = "Иванов"};
+            Student student2 = new Student() { Age = 21, Id = 5, GroupNumber = 32, Name = "Алексей", Surname = "Фралов"};
+
+            student1.Add(student2);
+            student2.AddThree(student1, student2);
+            Console.WriteLine("Student1:");
+            Console.WriteLine($"Id = {student1.Id}, Age = {student1.Age}, Name = {student1.Name}, Surname = {student1.Surname}, GroupNumber = {student1.GroupNumber}");
+            Console.WriteLine("Student2:");
+            Console.WriteLine($"Id = {student2.Id}, Age = {student2.Age}, Name = {student2.Name}, Surname = {student2.Surname}, GroupNumber = {student2.GroupNumber}");
+        }
+    }
+}
---
+++ lab1/lab1.2/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._2
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public int GroupNumber { get; set; }
+
+        public void Add(Student s)
+        {
+            Id += s.Id;
+            Name += s.Name;
+            Surname += s.Surname;
+            Age += s.Age;
+            GroupNumber += s.GroupNumber;
+        }
+        public void AddThree(Student s1, Student s2)
+        {
+            Id += s1.Id;
+            Name += s1.Name;
+            Surname += s1.Surname;
+            Age += s1.Age;
+            GroupNumber += s1.GroupNumber;
+
+            Id += s2.Id;
+            Name += s2.Name;
+            Surname += s2.Surname;
+            Age += s2.Age;
+            GroupNumber += s2.GroupNumber;
+        }
+    }
+}
